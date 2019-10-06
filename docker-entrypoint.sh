696685bf31dafa1f8bcf807f4c39684ee44d4f84
Maxime VISONNEAU <maxime.visonneau@gmail.com>
2019-10-06 23:39:51 +0100

    Removed mysql passwords of the docker compose example

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -e
 
 echo "-> checking environment variables"
 test ${GD_DB_HOSTNAME}
 test ${GD_DB_NAME}
 test ${GD_DB_USERNAME}
-test ${GD_DB_PASSWORD}
 export GD_DB_TABLE_PREFIX=${GD_DB_TABLE_PREFIX:-"gd_"}
 export GD_ENCRYPTION_SALT=${GD_ENCRYPTION_SALT:-"changeme"}
 export GD_API_ENABLED=${GD_API_ENABLED:-"true"}
